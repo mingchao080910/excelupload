d0a15043cea8f42c67932337d1cc85257a02bbfc
mingchao080910 <mingchao00@gmail.com>
2024-07-15 15:11:56 +0800

    asd

--- excel-upload-server/src/main.ts
+++ excel-upload-server/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  console.log(join(__dirname,"../browser"))
+  app.useStaticAssets(join(__dirname,"../browser"));  
+
+  // E:\angularProject\ExcelUpload\dist\excel-upload
   await app.listen(3000);
 }
 bootstrap();
