c33a1af485368b42ff9fe3594d0d44293d1d481b
mingchao080910 <mingchao00@gmail.com>
2024-08-07 15:30:08 +0800

    upload refresh add links

--- excel-upload-server/src/app.module.ts
+++ excel-upload-server/src/app.module.ts
@@ -15,11 +15,11 @@ import { Band } from './band/entities/band.entity';
     FileManagementModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: +process.env.MYSQL_PORT,
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
+      host: 'localhost',
+      port: 3306,
+      username: "root",
+      password: "aaa...000",
+      database: 'test',
       entities: [FileManagement, Phone, Feature, Band],
       synchronize: true,
       // logging: true
